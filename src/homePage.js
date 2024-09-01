0651a29bb50101b02b015c4a8ef6e38555c3cf68
AlejandroMullerMoreno <alejandromorenomuller@gmail.com>
2024-09-01 11:09:57 -0500

    move div so that it shows up as centered content

--- src/homePage.js
+++ src/homePage.js
@@ -7,14 +7,19 @@ function generateHomePageContent(){
     console.log(homePageMainContent);
 
     // add content/styling
-    homePageMainContent.innerHTML= "This is test content";
+    homePageMainContent.innerHTML= "This is test content. Nay, this IS content. Indeed, one might think that just becuase they say this or they say that that this isn't VALUE. Let me teach you how the world work, kid- entertainment and information are one and the same in the 21st century. Indeed, for better or for worse! You would be wise to remember that, my child. Now go... I have to read a Murakami novel....";
     homePageMainContent.id="contentSection";
     homePageMainContent.style.backgroundColor="#00308F";
     homePageMainContent.style.color="white";
     homePageMainContent.style.width="50vw";
+    homePageMainContent.style.height="75vh";
     homePageMainContent.style.zIndex="1";
-    homePageMainContent.style.display="flexbox";
-    homePageMainContent.style.justifyContent="center";
+    homePageMainContent.style.position="absolute";
+    homePageMainContent.style.top="100px";
+    homePageMainContent.style.left="25vw";
+    // homePageMainContent.style.display="flexbox";
+    // homePageMainContent.style.justifyContent="center";
+    // homePageMainContent.style.marginBottom="1000px";
 
     // append the div to the homepage
     mainContent.appendChild(homePageMainContent);
--- src/pageLoad.js
+++ src/pageLoad.js
@@ -7,6 +7,7 @@ console.log(mainContent);
 // add waifu background
 let waifuBackground = document.createElement("img");
 waifuBackground.src = "anime-waifu.jpg";
+waifuBackground.style.position="relative";
 mainContent.appendChild(waifuBackground);
 }
 
